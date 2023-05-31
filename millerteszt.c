19b6d6409d0fdbdd1e75131027cee2965c1b65ee
FLilian <91611020+FLilian@users.noreply.github.com>
2023-05-31 11:28:48 +0200

    Update millerteszt.c

--- millerteszt.c
+++ millerteszt.c
@@ -1,80 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
 
-// Gyors hatványozás moduláris aritmetikával
-unsigned long long fastModExp(unsigned long long base, unsigned long long exp, unsigned long long mod) {
-    unsigned long long result = 1;
-    base = base % mod;
+// Miller-Rabin prÃ­mteszt
+bool millerRabinTest(int number, int iterations) {
+    if (number < 2)
+        return false;
 
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            result = (result * base) % mod;
-        }
-        base = (base * base) % mod;
-        exp = exp / 2;
-    }
+    if (number != 2 && number % 2 == 0)
+        return false;
 
-    return result;
-}
-
-// Miller-Rabin prímszámteszt
-int millerRabin(unsigned long long n, int k) {
-    if (n <= 1) {
-        return 0;
-    }
-    if (n <= 3) {
-        return 1;
-    }
-    if (n % 2 == 0) {
-        return 0;
-    }
-
-    unsigned long long r = 0;
-    unsigned long long d = n - 1;
-    while (d % 2 == 0) {
-        r++;
+    int d = number - 1;
+    while (d % 2 == 0)
         d /= 2;
-    }
 
-    for (int i = 0; i < k; i++) {
-        unsigned long long a = 2 + rand() % (n - 3);
-        unsigned long long x = fastModExp(a, d, n);
+    for (int i = 0; i < iterations; ++i) {
+        int a = rand() % (number - 2) + 2;
+        int x = static_cast<int>(std::pow(a, d)) % number;
 
-        if (x == 1 || x == n - 1) {
+        if (x == 1 || x == number - 1)
             continue;
-        }
 
-        int prime = 0;
-        for (unsigned long long j = 0; j < r - 1; j++) {
-            x = fastModExp(x, 2, n);
-            if (x == n - 1) {
-                prime = 1;
+        bool isProbablePrime = false;
+        while (d != number - 1) {
+            x = (x * x) % number;
+            d *= 2;
+
+            if (x == 1)
+                return false;
+
+            if (x == number - 1) {
+                isProbablePrime = true;
                 break;
             }
         }
 
-        if (prime == 0) {
-            return 0;
-        }
+        if (!isProbablePrime)
+            return false;
     }
 
-    return 1;
+    return true;
 }
 
 int main() {
-    unsigned long long number;
-    int iterations;
-    printf("Adj meg egy szamot: ");
-    scanf_s("%llu", &number);
-    printf("Adj meg egy iteracios szamot: ");
-    scanf_s("%d", &iterations);
+    std::vector<int> numbers = { 123457, 123459, 123461, 123463 };  // TesztelendÅ‘ szÃ¡mok
+    int iterations = 10;  // IterÃ¡ciÃ³k szÃ¡ma
 
-    if (millerRabin(number, iterations)) {
-        printf("%llu primszam.\n", number);
-    }
-    else {
-        printf("%llu nem primszam.\n", number);
-    }
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, numbers.size()), [&](const tbb::blocked_range<size_t>& range) {
+        for (size_t i = range.begin(); i != range.end(); ++i) {
+            bool isPrime = millerRabinTest(numbers[i], iterations);
+
+            if (isPrime)
+                std::cout << numbers[i] << " is prime." << std::endl;
+            else
+                std::cout << numbers[i] << " is not prime." << std::endl;
+        }
+        });
 
     return 0;
 }
