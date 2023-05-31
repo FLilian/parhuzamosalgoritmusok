eab708dab4b20dec7b9fe09a7bddb66660572af2
FLilian <91611020+FLilian@users.noreply.github.com>
2023-05-31 11:30:09 +0200

    Update Solovay–Strassen_teszt.c

--- Solovay–Strassen_teszt.c
+++ Solovay–Strassen_teszt.c
@@ -1,95 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <omp.h>
 
-// Gyors hatv�nyoz�s modul�ris aritmetik�val
-unsigned long long fastModExp(unsigned long long base, unsigned long long exp, unsigned long long mod) {
-    unsigned long long result = 1;
-    base = base % mod;
+// Segédfüggvény az a^b mod n számításához
+int modularExponentiation(int a, int b, int n) {
+    int result = 1;
+    a = a % n;
 
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            result = (result * base) % mod;
+    while (b > 0) {
+        if (b & 1) {
+            result = (result * a) % n;
         }
-        base = (base * base) % mod;
-        exp = exp / 2;
+
+        a = (a * a) % n;
+        b = b >> 1;
     }
 
     return result;
 }
 
-// Jacobi-szimb�lum kisz�m�t�sa
-int jacobiSymbol(unsigned long long a, unsigned long long n) {
-    if (a == 0) {
-        return 0;
-    }
-    if (a == 1) {
-        return 1;
-    }
+// Segédfüggvény az Euler phi függvény kiszámításához
+int eulerPhi(int n) {
+    int result = n;
 
-    int result = 1;
-    while (a != 0) {
-        while (a % 2 == 0) {
-            a /= 2;
-            unsigned long long nmod8 = n % 8;
-            if (nmod8 == 3 || nmod8 == 5) {
-                result = -result;
+    for (int p = 2; p * p <= n; ++p) {
+        if (n % p == 0) {
+            while (n % p == 0) {
+                n = n / p;
             }
-        }
-        unsigned long long temp = a;
-        a = n % temp;
-        n = temp;
-        if (a > n / 2) {
-            a = a - n;
-        }
-        if (a % 4 == n % 4 && a % 4 == 3) {
-            result = -result;
+
+            result = result - (result / p);
         }
     }
 
-    if (n == 1) {
-        return result;
+    if (n > 1) {
+        result = result - (result / n);
     }
-    return 0;
+
+    return result;
 }
 
-// Solovay-Strassen pr�msz�mteszt
-int solovayStrassen(unsigned long long n, int k) {
-    if (n <= 1) {
-        return 0;
-    }
-    if (n <= 3) {
-        return 1;
-    }
-    if (n % 2 == 0) {
-        return 0;
-    }
+// Solovay-Strassen prímteszt
+bool solovayStrassenTest(int number, int iterations) {
+    if (number < 2) return false;
+    if (number != 2 && number % 2 == 0) return false;
 
-    for (int i = 0; i < k; i++) {
-        unsigned long long a = 2 + rand() % (n - 3);
-        unsigned long long x = fastModExp(a, (n - 1) / 2, n);
+    for (int i = 0; i < iterations; ++i) {
+        int a = rand() % (number - 2) + 2; // Véletlenszerű a választása [2, number-1] között
 
-        if (x != 1 && x != n - 1 && jacobiSymbol(a, n) != x % n) {
-            return 0;
+        int x = modularExponentiation(a, (number - 1) / 2, number);
+
+        if (x != 1 && x != number - 1) {
+            return false; // Nem prím
+        }
+
+        int jacobiSymbol = (x == 1) ? 1 : -1;
+
+        if (jacobiSymbol != (modularExponentiation(a, (number - 1) / 2, number) % number)) {
+            return false; // Nem prím
         }
     }
 
-    return 1;
+    return true; // Valószínűleg prím
 }
 
 int main() {
-    unsigned long long number;
-    int iterations;
-    printf("Adj meg egy szamot: ");
-    scanf_s("%llu", &number);
-    printf("Adj meg egy iteracios szamot: ");
-    scanf_s("%d", &iterations);
+    int number = 100; // Tesztelendő szám
+    int iterations = 10; // Iterációk száma
 
-    if (solovayStrassen(number, iterations)) {
-        printf("%llu primszam.\n", number);
+    bool isPrime = true;
+
+#pragma omp parallel for shared(isPrime) num_threads(4)
+    for (int i = 2; i <= number; ++i) {
+        if (!solovayStrassenTest(i, iterations)) {
+#pragma omp critical
+            {
+                isPrime = false;
+            }
+        }
+    }
+
+    if (isPrime) {
+        printf("%d is prime.\n", number);
     }
     else {
-        printf("%llu nem primszam.\n", number);
+        printf("%d is not prime.\n", number);
     }
 
     return 0;
 }
+
